ffe3dc1b8413eba9556e063df791ec69cf79a21d
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-06 09:53:18 +0900

    建立model Memo

---
+++ app/src/main/java/com/example/memoapp_firebase_practice/model/Memo.kt
@@ -0,0 +1,9 @@
+// Memo.kt
+package com.example.memoapp_firebase_practice.model
+
+data class Memo(
+    var id: String = "",          // Firestore Document ID
+    var title: String = "",
+    var content: String = "",
+    var timestamp: Long = 0L      // 時間戳（儲存為毫秒）
+)
