d6b4dddb25eb0894a9bb0f81c6bf17047a32d514
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-06 09:58:08 +0900

    建立/初始化MemoListActivity

---
+++ app/src/main/java/com/example/memoapp_firebase_practice/MemoListActivity.kt
@@ -0,0 +1,20 @@
+package com.example.memoapp_firebase_practice
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MemoListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_memo_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
