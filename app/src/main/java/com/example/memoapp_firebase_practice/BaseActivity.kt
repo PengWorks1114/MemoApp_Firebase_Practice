db8102d81e9d51b0a8b753dadb3b9d520613c2a8
peng0o0 <tomato.202504.penghc@gmail.com>
2025-08-06 15:24:12 +0900

    add BaseActivity() {

---
+++ app/src/main/java/com/example/memoapp_firebase_practice/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.example.memoapp_firebase_practice
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+    override fun attachBaseContext(newBase: Context) {
+        val lang = LocaleHelper.getCurrentLanguage()
+        val context = LocaleHelper.setLocale(newBase, lang)
+        super.attachBaseContext(context)
+    }
+}
